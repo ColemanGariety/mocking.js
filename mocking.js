874072ce16333368c517a57b7869cacd1ad92cca
Jackson Gariety <personal@jacksongariety.com>
2013-08-14 00:45:36 -0700

    Adds sorting functions and starts on the random replacement bits. Also adds config.json support.

--- mocking.js
+++ mocking.js
@@ -1,31 +1,74 @@
-var Twit = require('twit'),
+// Dependencies
+var Twitter = require('twit'),
     Pos = require('pos'),
-    consumer_key = 'BPIzUvAYt42eASw0uZeOJg',
-    consumer_secret = 'Tkd7BpS1a193j1pYsDBjszHypTeUojEnbL5qrzts'
+    config = require('./config.json')
 
-// Connect to the human's account
-marco = new Twit({
-  consumer_key:        consumer_key,
-  consumer_secret:     consumer_secret,
-  access_token:        '248558843-dr1pQWqzvinSyM6RQt7lUomNsYuIJJEVDJ7CsL5M',
-  access_token_secret: '3Q7aOd5bLlamPOS7JZtfnQrF7Btdby6YasiLfVOvGg'
-})
+// Connect to both marco's account and polo's account
+var marco = new Twitter({ consumer_key: config.consumer_key, consumer_secret: config.consumer_secret, access_token: config.marco_access_token, access_token_secret: config.marco_access_token_secret }),
+    polo = new Twitter({ consumer_key: config.consumer_key, consumer_secret: config.consumer_secret, access_token: config.polo_access_token, access_token_secret: config.polo_access_token_secret })
 
-// Connect to the mocking jay's account
-polo = new Twit({
-  consumer_key:        consumer_key,
-  consumer_secret:     consumer_secret,
-  access_token:        '248558843-dr1pQWqzvinSyM6RQt7lUomNsYuIJJEVDJ7CsL5M',
-  access_token_secret: '3Q7aOd5bLlamPOS7JZtfnQrF7Btdby6YasiLfVOvGg'
+// Grab Marco's last 10 tweets (using polo's account to exclude replies)
+polo.get('statuses/user_timeline', { screen_name: 'JacksonGariety', count: 10, exclude_replies: true }, function(err, tweets) {
+  var tweet = tweets.shift(),
+      i = tweets.length
+  
+  if (i) {
+    var adjectives = [],
+        nouns = [],
+        verbs = []
+    
+    // A simple script to get a random word by its part of speech
+    function getRandom(pos) {
+      var index = pos[Math.floor(Math.random() * pos.length)]
+      return pos[index]
+    }
+    
+    // Loop through the words in the historic tweets
+    while (i--) {
+      var lexicon = new Pos.Lexer().lex(tweets[i].text),
+          words = new Pos.Tagger().tag(lexicon),
+          j = words.length
+      
+      while (j--) {
+        var tag = words[j],
+            word = tag[0],
+            part = tag[1]
+        
+        // Sort the array by part of speech
+        if (part == "JJ") { adjectives.push(word) }
+        else if (part == "NN" || part == "NNP") { nouns.push(word) }
+        else if (part == "VB") { verbs.push(word) }
+      }
+    }
+    
+    // Loop through words in the present tweet
+    var lexicon = new Pos.Lexer().lex(tweet.text),
+        words = new Pos.Tagger().tag(lexicon),
+        k = words.length
+    
+    while (k--) {
+      var tag = words[k],
+          word = tag[0],
+          part = tag[1]
+      
+      // Swap out words with a bit of randomization
+      if (Math.random() <= .25) {
+        var pos
+      
+        if (part == "JJ") { pos = adjectives }
+        else if (part == "NN" || part == "NNP") { pos = nouns }
+        else if (part == "VB") { pos = verbs }
+        
+        words[k] = pos[Math.floor(Math.random() * adjectives.length)]
+      }
+    }
+  }
+  
+  // Polo tweets a new message
 })
 
 // Listen for marco's tweets
 var listen = marco.stream('user')
 listen.on('tweet', function(tweet) {
-  // Grab marco's last 10 tweets
 
-  var words = new Pos.Lexer().lex(tweet.text),
-      tagged = new Pos.Tagger().tag(words)
-  
-  console.log(tagged)
 })
