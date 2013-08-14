4ce3a2372856775bbc3d4d7ba066616e112f92fe
Jackson Gariety <personal@jacksongariety.com>
2013-08-14 00:52:02 -0700

    Adds randomization swapping, de-lexifying, and tweeting the modified tweet.

--- mocking.js
+++ mocking.js
@@ -7,68 +7,77 @@ var Twitter = require('twit'),
 var marco = new Twitter({ consumer_key: config.consumer_key, consumer_secret: config.consumer_secret, access_token: config.marco_access_token, access_token_secret: config.marco_access_token_secret }),
     polo = new Twitter({ consumer_key: config.consumer_key, consumer_secret: config.consumer_secret, access_token: config.polo_access_token, access_token_secret: config.polo_access_token_secret })
 
-// Grab Marco's last 10 tweets (using polo's account to exclude replies)
-polo.get('statuses/user_timeline', { screen_name: 'JacksonGariety', count: 10, exclude_replies: true }, function(err, tweets) {
-  var tweet = tweets.shift(),
-      i = tweets.length
-  
-  if (i) {
-    var adjectives = [],
-        nouns = [],
-        verbs = []
-    
-    // A simple script to get a random word by its part of speech
-    function getRandom(pos) {
-      var index = pos[Math.floor(Math.random() * pos.length)]
-      return pos[index]
-    }
-    
-    // Loop through the words in the historic tweets
-    while (i--) {
-      var lexicon = new Pos.Lexer().lex(tweets[i].text),
-          words = new Pos.Tagger().tag(lexicon),
-          j = words.length
-      
-      while (j--) {
-        var tag = words[j],
-            word = tag[0],
-            part = tag[1]
-        
-        // Sort the array by part of speech
-        if (part == "JJ") { adjectives.push(word) }
-        else if (part == "NN" || part == "NNP") { nouns.push(word) }
-        else if (part == "VB") { verbs.push(word) }
-      }
-    }
-    
-    // Loop through words in the present tweet
-    var lexicon = new Pos.Lexer().lex(tweet.text),
-        words = new Pos.Tagger().tag(lexicon),
-        k = words.length
-    
-    while (k--) {
-      var tag = words[k],
-          word = tag[0],
-          part = tag[1]
-      
-      // Swap out words with a bit of randomization
-      if (Math.random() <= .25) {
-        var pos
-      
-        if (part == "JJ") { pos = adjectives }
-        else if (part == "NN" || part == "NNP") { pos = nouns }
-        else if (part == "VB") { pos = verbs }
-        
-        words[k] = pos[Math.floor(Math.random() * adjectives.length)]
-      }
-    }
-  }
-  
-  // Polo tweets a new message
-})
-
 // Listen for marco's tweets
 var listen = marco.stream('user')
 listen.on('tweet', function(tweet) {
-
+  // Grab Marco's last 10 tweets (using polo's account to exclude replies)
+  polo.get('statuses/user_timeline', { screen_name: 'JacksonGariety', count: 10, exclude_replies: true }, function(err, tweets) {
+    var tweet = tweets.shift(),
+        i = tweets.length
+    
+    if (i) {
+      var adjectives = [],
+          nouns = [],
+          verbs = []
+      
+      // A simple script to get a random word by its part of speech
+      function getRandom(pos) {
+        var index = pos[Math.floor(Math.random() * pos.length)]
+        return pos[index]
+      }
+      
+      // Loop through the words in the historic tweets
+      while (i--) {
+        var lexicon = new Pos.Lexer().lex(tweets[i].text),
+            words = new Pos.Tagger().tag(lexicon),
+            j = words.length
+        
+        while (j--) {
+          var tag = words[j],
+              word = tag[0],
+              part = tag[1]
+          
+          // Sort the array by part of speech
+          if (part == "JJ") { adjectives.push(word) }
+          else if (part == "NN" || part == "NNP") { nouns.push(word) }
+          else if (part == "VB") { verbs.push(word) }
+        }
+      }
+      
+      // Loop through words in the present tweet
+      var lexicon = new Pos.Lexer().lex(tweet.text),
+          words = new Pos.Tagger().tag(lexicon),
+          k = words.length
+      
+      while (k--) {
+        var tag = words[k],
+            word = tag[0],
+            part = tag[1]
+        
+        // Swap out words with a bit of randomization
+        if (Math.random() <= .25) {
+          var pos
+        
+          if (part == "JJ") { pos = adjectives }
+          else if (part == "NN" || part == "NNP") { pos = nouns }
+          else if (part == "VB") { pos = verbs }
+          
+          words[k] = pos[Math.floor(Math.random() * adjectives.length)]
+        }
+      }
+      
+      // De-lexify it
+      text = words.join(" ")
+      
+      // Polo tweets a new message
+      polo.post('statueses/update', { status: text }, function(err, reply) {
+        if (err) {
+          console.log(err)
+          return false
+        }
+        
+        console.log("Tweeting... \n" + reply)
+      })
+    }
+  })
 })
