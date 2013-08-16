d1d57776c7b687662f9ea8b15c06c878f99eddbf
Jackson Gariety <personal@jacksongariety.com>
2013-08-15 23:02:51 -0700

    Now grabs random users. Blacklist is integrated too! Way nicer tweaks to the randomization, and all around better tweets coming out of polo. I'm very happy with the reuslts.

--- mocking.js
+++ mocking.js
@@ -9,78 +9,80 @@ var marco = new Twitter({ consumer_key: config.consumer_key, consumer_secret: co
     polo = new Twitter({ consumer_key: config.consumer_key, consumer_secret: config.consumer_secret, access_token: config.polo_access_token, access_token_secret: config.polo_access_token_secret })
 
 // Setup empty arrays to store parts of speech (staying global for a neat mapping hack)
-var parts = { "JJ": [], "JJR": [], "JJS": [], "NN": [], "NNP": [], "NNPS": [], "NNS": [], "RB": [], "RBR": [], "RBS": [], "RP": [], "VB": [], "VBD": [], "VBG": [], "VBN": [], "VBP": [], "VBZ": [] }
-    
-// A helpful public method for flattening multidimensional arrays
-var flatten = function(array){
-  var flat = [];
-  for (var i = 0, l = array.length; i < l; i++){
-    var type = Object.prototype.toString.call(array[i]).split(' ').pop().split(']').shift().toLowerCase();
-    if (type) { flat = flat.concat(/^(array|collection|arguments|object)$/.test(type) ? flatten(array[i]) : array[i]); }
-  }
-  return flat;
-}
+var parts = { "JJ": [], "NN": [], "NNP": [], "NNPS": [], "NNS": [], "VB": [], "VBG": [], "VBN": [], "RB": [], "RBR": [], "RBS": [] },
+    people = [],
+    blacklist = ["i", "a", ".a", "it's", "rt", "am", "pm", "mt", "an", "the"],
+    mention = /(^|\W+)\@([\w\-]+)/gm
 
 // Listen for marco's tweets
 var listen = marco.stream('user')
 listen.on('tweet', function(tweet) {
-  if (tweet.user.screen_name == marco.screen_name && tweet.text.substring(0,18) != "RT @" + config.polo_screen_name) {
+  if (tweet.user.screen_name == config.marco_screen_name && tweet.text.substring(0,18) != "RT @" + config.polo_screen_name) {
     // Grab Marco's last 25 tweets (using polo's account to exclude replies)
     polo.get('statuses/user_timeline', { screen_name: config.marco_screen_name, count: 25, exclude_replies: false }, function(err, tweets) {
+      if (err) {
+        console.log(err)
+        return false
+      }
+
       var tweet = tweets.shift(),
           i = tweets.length
       
       if (i) {
-        // Loop through the words in the historic tweets
-        while (i--) {
+        while (i--) { // Loop through historic tweets
           var lexicon = new Pos.Lexer().lex(tweets[i].text),
               words = new Pos.Tagger().tag(lexicon),
               j = words.length
           
-          while (j--) {
+          while (j--) { // Loop through the words in the historic tweets
             var tag = words[j],
                 word = tag[0],
                 part = tag[1]
             
             // Sort the array by part of speech
-            var pos = parts[part]
-            if (pos) {
-              if (word.match(/(^|\W+)\@([\w\-]+)/gm)) {
-                pos.push(config.people[Math.floor(Math.random() * config.people.length)])
-              } else if (config.blacklist.indexOf(word) == -1) {
-                pos.push(word)
-              }
+            if (parts[part] && blacklist.concat(parts[part]).indexOf(word.toLowerCase()) == -1 && !word.match(mention)) {
+              parts[part].push(word)
             }
           }
         }
-        
-        // Loop through words in the present tweet
+          
+        // Lexify & tag the present tweet
         var lexicon = new Pos.Lexer().lex(tweet.text),
             words = new Pos.Tagger().tag(lexicon),
-            k = words.length
+            l = words.length
         
-        while (k--) {
-          var tag = words[k],
-              word = tag[0],
-              part = tag[1]
-          
-          // Swap out words with a bit of randomization
-          var pos = parts[part]
-          if ([".", ",", ";", "—", '!', ':'].indexOf(word) != -1) {
-            words[words.indexOf(tag) - 1][0] += word
-            words[words.indexOf(tag)] = ["", ""]
-          } else if (words[k][0].match(/(^|\W+)\@([\w\-]+)/gm)) {
-            tweet.text.replace(words[k][0], config.people[Math.floor(Math.random() * config.people.length)])
-          } else if (Math.random() <= .33 && pos) {
-            // Get a random word by part of speech and replace the original tweet with it
-            tweet.text.replace(words[k][0], pos[Math.floor(Math.random() * pos.length)]
+        // Search for relevant tweets
+        polo.get('search/tweets', { q: parts["NN"][Math.floor(Math.random() * parts["NN"].length)], count: 46 }, function(err, res) {
+          if (err) {
+            console.log(err)
+            return false
           }
-        }
-        
-        // Polo tweets a new message
-        console.log("Tweeting... \n" + text)
-        polo.post('statuses/update', { status: tweet.text }, function(err, reply) {
-          if (err) console.log(err)
+
+          var tweets = res.statuses,
+              k = tweets.length
+          while (k--) { // Loop over the tweets and save the users
+            people.push(tweets[k].user.screen_name)
+          }
+          
+          
+          while (l--) { // Loop through words in the present tweet
+            var tag = words[l],
+                word = tag[0],
+                part = tag[1]
+            
+            // Swap out words with a bit of randomization
+            var pos = parts[part]
+            if (words[l][0].match(mention)) { // If it's an @mention change it to one of the users we grabbed before
+              tweet.text = tweet.text.replace(words[l][0], "@" + people[Math.floor(Math.random() * people.length)])
+            } else if (Math.random() <= .66 && pos) {
+              // Get a random word by part of speech and replace the original tweet with it
+              tweet.text = tweet.text.replace(words[l][0], pos[Math.floor(Math.random() * pos.length)])
+            }
+          }
+          
+          // Polo tweets a new message
+          console.log("Tweeting... \n" + tweet.text)
+          polo.post('statuses/update', { status: tweet.text }, function(err, reply) { if (err) console.log(err)  })
         })
       }
     })
